59e707ab766baea10d76feefb6e9f12c9d5b9275
Scott Mobley <Zott820@yahoo.com>
2014-11-15 04:47:16 -0800

    Finished Text Box

---
+++ Assets/CollisionDetect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log(coll.collider.gameObject.name);
+		if (coll.collider.gameObject.name =="Front" ) {
+			Debug.Log("You hugged the front");
+	} else
+			Debug.Log("You going Touch jail son");
+}
+}
--- Assets/PersonSpeech.cs
+++ Assets/PersonSpeech.cs
@@ -22,7 +22,7 @@ public class PersonSpeech : MonoBehaviour
 		void Update ()
 		{
 				if (Input.GetKeyDown ("space")) {
-						playVoice (1, "Reject");
+						playVoice (1, "Acceptance");
 				}
 				//For testing only"
 
@@ -41,6 +41,8 @@ public class PersonSpeech : MonoBehaviour
 
 		public float bubbleDelay = 2f;
 		public float bubblePopSpeed = 2f;
+		public float textSizeStart = 125f;
+		public float textSizeModifier = 5f;
 
 		void showBubble ()
 		{ 
@@ -58,10 +60,18 @@ public class PersonSpeech : MonoBehaviour
 				string directory = "Audio/Person_" + personVoice + "/" + action + "/";
 				clips = Resources.LoadAll<AudioClip> (directory);
 				int chosenClip = Random.Range (0, clips.Length);
+				int clipIndex = int.Parse (clips [chosenClip].name) - 1;
+				//The freaking clips are not ordered alphabetically.....grab the line by the name of the audio file *sigh, the fools.
+
 				soundManager.clip = clips [chosenClip];   //Set the clip to a random audio file in the directory.
 				soundManager.Play ();
+				string allLines = Resources.Load<TextAsset> (directory + "strings").text;
+				string[] linesInFile = allLines.Split ('\n');
 
-				speechbubbleText.text = Resources.Load<TextAsset> (directory + 1).text;
+				if (clipIndex <= linesInFile.Length) {
+						speechbubbleText.text = ResolveTextSize (linesInFile [clipIndex], bubbleLength);
+						speechbubbleText.fontSize = (int)Mathf.Max ((textSizeStart - speechbubbleText.text.Length * textSizeModifier), 35);
+				}
 
 				//Wait until audio is done playing before unloading it.
 				yield return new WaitForSeconds (soundManager.clip.length);
@@ -83,6 +93,39 @@ public class PersonSpeech : MonoBehaviour
 				}
 				speechbubble.transform.localScale = end;
 				bubbleStatus = finalStatus;
+		}
+
+		public int bubbleLength;
+		//http://answers.unity3d.com/questions/190800/wrapping-a-textmesh-text.html
+		// Wrap text by line height
+		private string ResolveTextSize (string input, int lineLength)
+		{
+				// Split string by char " "
+				string[] words = input.Split (" " [0]);
+				// Prepare result
+				string result = "";
+				// Temp line string
+				string line = "";
+				// for each all words
+				foreach (string s in words) {
+						// Append current word into line
+						string temp = line + " " + s;
+						// If line length is bigger than lineLength
+						if (temp.Length > lineLength) {
+								// Append current line into result
+								result += line + "\n";
+								// Remain word append into new line
+								line = s;
+						}
+			// Append current word into current line
+			else {
+								line = temp;
+						}
+				}
+				// Append last line into result
+				result += line;
+				// Remove first " " char
+				return result.Substring (1, result.Length - 1);
 		}
 
 }
